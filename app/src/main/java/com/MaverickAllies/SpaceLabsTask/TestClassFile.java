afb3c2015ad73696d1338c50df5fa5769c72810a
visha <vishaldevasics@gmail.com>
2023-11-15 10:47:07 +0530

    TestCommit

---
+++ app/src/main/java/com/MaverickAllies/SpaceLabsTask/TestClassFile.java
@@ -0,0 +1,14 @@
+package com.MaverickAllies.SpaceLabsTask;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TestClassFile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_class_file);
+    }
+}
